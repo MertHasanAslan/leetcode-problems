f1adb58533fb06cac64928da912e4e923a56801d
merthasanaslan <merthasanaslan325@gmail.com>
2025-07-17 09:18:51 +0300

    Minumum Stack Solved

---
+++ neetcode_problems/Minimum Stack.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+public:
+    std::stack<int> stack;
+    std::stack<int> min_stack;
+    MinStack() {    }
+    
+    void push(int val) {
+        stack.push(val);
+        if (min_stack.empty()){ min_stack.push(val); }
+        else{
+            if (val < min_stack.top()) { min_stack.push(val); }
+            else { min_stack.push(min_stack.top()); }
+        }
+    }
+    
+    void pop() {
+        stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
