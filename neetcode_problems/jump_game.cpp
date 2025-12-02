75c5f2e484cb1051553aac30ab54f636c1aac689
MertHasanAslan <merthasanaslan325@gmail.com>
2025-12-02 21:41:11 +0300

    jump game solved by using greedy way

---
+++ neetcode_problems/jump_game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    unordered_map<int, bool> m;
+public:
+    bool canJump(vector<int>& nums) {
+        int target = nums.size()-1;
+
+        for(int i = nums.size()-1; i >= 0; i--){
+            if(i + nums[i] >= target){
+                target = i;
+            }
+        }
+
+        return target == 0;
+
+    }
+
+};
